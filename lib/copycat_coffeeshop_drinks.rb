7c442de20f2ad46a902f706307fccc4fbef85dc7
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2019-01-14 16:20:50 +0000

    modified lib/copycat_coffeeshop_drinks.rb

--- lib/copycat_coffeeshop_drinks.rb
+++ lib/copycat_coffeeshop_drinks.rb
@@ -2,17 +2,3 @@ module CopycatCoffeeshopDrinks
 end
 
 require_relative '../config/environment'
-
-
-
-
-
-
-#require_relative "./copycat_coffeeshop_drinks/version"
-
-#module CopycatCoffeeshopDrinks
-  #class Error < StandardError; end
-  # Your code goes here...
-#end
-
-#require_relative './copycat_coffeeshop_drinks/cli'
