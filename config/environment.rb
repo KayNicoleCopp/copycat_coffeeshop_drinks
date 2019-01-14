f34e2fb306482f87711bdd19ff730c952b0a2f20
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2019-01-14 15:56:56 +0000

    made changes to environment.rb

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+
+require_relative '../lib/copycat_coffeeshop_drinks/version'
+require_relative '../lib/copycat_coffeeshop_drinks/scraper'
+require_relative '../lib/copycat_coffeeshop_drinks/pattern'
+require_relative '../lib/copycat_coffeeshop_drinks/cli'
