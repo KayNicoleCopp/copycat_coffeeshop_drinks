b8e8c69d1cdcc3652162b190c3b041271684d204
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2019-02-01 22:18:43 +0000

    commented everything out

--- lib/copycat_coffeeshop_drinks/cli.rb
+++ lib/copycat_coffeeshop_drinks/cli.rb
@@ -1,44 +1,44 @@
-# our CLI Controller
-
-class CopycatCoffeeshopDrinks::CLI
-
-  def call  #instance method
-    puts "Welcome Fellow Coffee Lovers!"
-    puts "Please choose 1 of my 5 favorite coffee drinks from the list below that you'd like to make!"
-    puts "Type either 'creamy caramel mocha', 'holiday peppermint mocha', 'creamy irish coffee', 'iced coffee latte', or 'hazelnut coffee'"
-    input = gets.strip.downcase
-    case input
-    when "creamy caramel mocha"
-      puts "in creamy caramel mocha"
-      #Scrape the creamy caramel mocha page
-      url = "https://www.tasteofhome.com/recipes/creamy-caramel-mocha/"
-      CopycatCoffeeshopDrinks::Scraper.scrape_drinks(url)
-    when "holiday peppermint mocha"
-      puts "in holiday peppermint mocha"
-      #Scrape the holiday peppermint mocha page
-      url = "https://www.tasteofhome.com/recipes/holiday-peppermint-mocha/"
-      CopycatCoffeeshopDrinks::Scraper.scrape_drinks(url)
-    when "creamy irish coffee"
-      puts "in creamy irish coffee"
-      #Scrape the creamy irish coffee page
-      url = "https://www.tasteofhome.com/recipes/creamy-irish-coffee/"
-      CopycatCoffeeshopDrinks::Scraper.scrape_drinks(url)
-    when "iced coffee latte"
-      puts "in iced coffee latte"
-      #Scrape the iced coffee latte page
-      url = "https://www.tasteofhome.com/recipes/iced-coffee-latte/"
-      CopycatCoffeeshopDrinks::Scraper.scrape_drinks(url)
-    when "hazelnut coffee"
-      puts "in hazelnut coffee"
-      #Scrape the hazelnut coffee page
-      url = "https://www.tasteofhome.com/recipes/hazelnut-coffee/"
-      CopycatCoffeeshopDrinks::Scraper.scrape_drinks(url)
-    when "exit"
-      puts "Goodbye!"
-    else
-      #they did not put in correct input
-      puts "Invalid!"
-    end
-  end
-
-end
+# # our CLI Controller
+#
+# class CopycatCoffeeshopDrinks::CLI
+#
+#   def call  #instance method
+#     puts "Welcome Fellow Coffee Lovers!"
+#     puts "Please choose 1 of my 5 favorite coffee drinks from the list below that you'd like to make!"
+#     puts "Type either 'creamy caramel mocha', 'holiday peppermint mocha', 'creamy irish coffee', 'iced coffee latte', or 'hazelnut coffee'"
+#     input = gets.strip.downcase
+#     case input
+#     when "creamy caramel mocha"
+#       puts "in creamy caramel mocha"
+#       #Scrape the creamy caramel mocha page
+#       url = "https://www.tasteofhome.com/recipes/creamy-caramel-mocha/"
+#       CopycatCoffeeshopDrinks::Scraper.scrape_drinks(url)
+#     when "holiday peppermint mocha"
+#       puts "in holiday peppermint mocha"
+#       #Scrape the holiday peppermint mocha page
+#       url = "https://www.tasteofhome.com/recipes/holiday-peppermint-mocha/"
+#       CopycatCoffeeshopDrinks::Scraper.scrape_drinks(url)
+#     when "creamy irish coffee"
+#       puts "in creamy irish coffee"
+#       #Scrape the creamy irish coffee page
+#       url = "https://www.tasteofhome.com/recipes/creamy-irish-coffee/"
+#       CopycatCoffeeshopDrinks::Scraper.scrape_drinks(url)
+#     when "iced coffee latte"
+#       puts "in iced coffee latte"
+#       #Scrape the iced coffee latte page
+#       url = "https://www.tasteofhome.com/recipes/iced-coffee-latte/"
+#       CopycatCoffeeshopDrinks::Scraper.scrape_drinks(url)
+#     when "hazelnut coffee"
+#       puts "in hazelnut coffee"
+#       #Scrape the hazelnut coffee page
+#       url = "https://www.tasteofhome.com/recipes/hazelnut-coffee/"
+#       CopycatCoffeeshopDrinks::Scraper.scrape_drinks(url)
+#     when "exit"
+#       puts "Goodbye!"
+#     else
+#       #they did not put in correct input
+#       puts "Invalid!"
+#     end
+#   end
+#
+# end
--- lib/copycat_coffeeshop_drinks/scraper.rb
+++ lib/copycat_coffeeshop_drinks/scraper.rb
@@ -2,8 +2,13 @@ class CopycatCoffeeshopDrinks::Scraper
 
   def self.scrape_drinks(url)
     webpage = Nokogiri::HTML(open(url))
-    #name = doc.css
+    ingredient_card = webpage.css("div.recipe-ingredients")
+
+    ingredient_card.map do |link|
+      binding.pry
+      CopycatCoffeeshopDrinks::Ingredients.new(link, text)
   end
+end
 
   def scrape_ingredients
   end
