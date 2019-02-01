e19ff9845e10fc4f67d1e1a1962f7bd2c64fd65b
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2019-02-01 19:58:55 +0000

    started scraping webpage

--- lib/copycat_coffeeshop_drinks/scraper.rb
+++ lib/copycat_coffeeshop_drinks/scraper.rb
@@ -1,9 +1,8 @@
 class CopycatCoffeeshopDrinks::Scraper
 
   def self.scrape_drinks(url)
-    #page = Nokogiri::HTML(open(url))
+    webpage = Nokogiri::HTML(open(url))
     #name = doc.css
-    puts "In the scraper class"
   end
 
   def scrape_ingredients
