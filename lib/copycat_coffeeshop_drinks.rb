6e14340d7d765fb48c6a6d0715053de9652e1ffb
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2019-02-01 19:03:41 +0000

    modified copycat_coffeeshop_drinks.rb

--- lib/copycat_coffeeshop_drinks.rb
+++ lib/copycat_coffeeshop_drinks.rb
@@ -5,3 +5,5 @@ require "pry"
 
 require_relative "copycat_coffeeshop_drinks/version"
 require_relative "copycat_coffeeshop_drinks/cli"
+require_relative "copycat_coffeeshop_drinks/scraper"
+require_relative "copycat_coffeeshop_drinks/drinks"
