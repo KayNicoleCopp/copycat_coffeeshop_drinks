e473b48259c47312ade3a7f4bb42302460f0d313
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2019-02-02 22:04:18 +0000

    changed scraper

--- lib/copycat_coffeeshop_drinks/scraper.rb
+++ lib/copycat_coffeeshop_drinks/scraper.rb
@@ -2,10 +2,9 @@ class CopycatCoffeeshopDrinks::Scraper
 
   def self.scrape_drinks
     webpage = Nokogiri::HTML(open("https://www.tasteofhome.com/collection/homemade-coffee-shop-drinks/view-all/"))
-binding.pry
     drinks = webpage.css("div.listicle-page")
     drinks.each do |drink_card|
-      drink = CopycatCoffeeshopDrinks::Drinks.new(title, url)
+      drink = CopycatCoffeeshopDrinks::Drinks.new
       drink.title = drink_card.css("h4.listicle-page__title a").text
     end
 
