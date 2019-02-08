6cf4a8675f0f0c37643ad196af9785a62b66d067
Kayla Coppernoll <kayla.coppernoll@yahoo.com>
2019-02-08 17:11:23 +0000

    modified main_menu

--- lib/copycat_coffeeshop_drinks/cli.rb
+++ lib/copycat_coffeeshop_drinks/cli.rb
@@ -22,13 +22,16 @@ class CopycatCoffeeshopDrinks::CLI
       puts "To see the recipe for a drink, enter the corresponding number below"
       input = gets.strip.downcase
     end
-
-    if input.to_i > 0 #&& <= all_drinks.size
+        if input.to_i > 0 #&& <= all_drinks.size
         CopycatCoffeeshopDrinks::Scraper.scrape_instruction_card
         #need to see ingredients and instructions
+        #elsif input == "exit"
+        #puts "Thanks for stopping by!"
 
-    end
-end
+        end
+     end
+
+     
   end
 
 
